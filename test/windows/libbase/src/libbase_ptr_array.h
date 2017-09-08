88e80058fb4b5a6f54a316634bfba944fac6fb47
Prabhu Sammandam <prabhusammandam@gmail.com>
2017-09-08 11:06:38 +0530

    renamed windows libbase test folder.

--- test/windows/libbase/src/libbase_ptr_array.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBBASE_PTR_ARRAY_H_
-#define LIBBASE_PTR_ARRAY_H_
-
-#include "cute_suite.h"
-
-extern cute::suite make_suite_libbase_ptr_array();
-
-#endif /* LIBBASE_PTR_ARRAY_H_ */
---
+++ test/windows/libbase_test/src/libbase_hash.h
@@ -0,0 +1,8 @@
+#ifndef LIBBASE_HASH_H_
+#define LIBBASE_HASH_H_
+
+#include "cute_suite.h"
+
+extern cute::suite make_suite_libbase_hash();
+
+#endif /* LIBBASE_HASH_H_ */
